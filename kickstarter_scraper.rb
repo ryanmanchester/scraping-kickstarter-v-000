bee620997ec786fe67e633d74b8ab75272590a4c
Ryan Manchester <ryanm17@gmail.com>
2018-02-06 23:14:13 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,4 @@ create_project_hash
 # image :  project.css("div.project-thumbnail a img").attribute("src").value
 # description : project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta").text
+# percent_funded : project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
