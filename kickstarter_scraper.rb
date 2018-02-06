3e31c98bee01abe49899d887110626f9c68acfce
Ryan Manchester <ryanm17@gmail.com>
2018-02-06 23:22:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,9 +9,9 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_ling => project.css("div.project-thumbnail a img").attribute("src").value
-      :description => description : project.css("p.bbcard_blurb").text
-      :location => roject.css("ul.project-meta").text
+      :image_ling => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => description : project.css("p.bbcard_blurb").text,
+      :location => roject.css("ul.project-meta").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
